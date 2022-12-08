73dcea576e40af237dc515ecdfd53fdb2022d9db
QUENTIN JONES <119446703+QuentinEJones@users.noreply.github.com>
2022-12-08 18:44:43 +0000

    Basic Form Change

--- Form.php
+++ Form.php
@@ -1,4 +1,4 @@
-<form action="submit.php" method="post">
+ <form action="submit.php" method="post">
   <label for="fname">First Name:</label><br>
   <input type="text" id="fname" name="fname"><br>
   <label for="lname">Last Name:</label><br>
@@ -6,6 +6,11 @@
   <label for="age">Age:</label><br>
   <input type="number" id="age" name="age"><br>
   <label for="gender">Gender:</label><br>
+  <select id="gender" name="gender">
+    <option value="male">Male</option>
+    <option value="female">Female</option>
+  </select><br><br>
+   <label for="gender">Realtion  :</label><br>
   <select id="gender" name="gender">
     <option value="male">Male</option>
     <option value="female">Female</option>
