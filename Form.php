128e05554a3f1b12da9805096dc64da9765479f4
QUENTIN JONES <119446703+QuentinEJones@users.noreply.github.com>
2022-12-08 18:42:58 +0000

    base Form

---
+++ Form.php
@@ -0,0 +1,14 @@
+<form action="submit.php" method="post">
+  <label for="fname">First Name:</label><br>
+  <input type="text" id="fname" name="fname"><br>
+  <label for="lname">Last Name:</label><br>
+  <input type="text" id="lname" name="lname"><br>
+  <label for="age">Age:</label><br>
+  <input type="number" id="age" name="age"><br>
+  <label for="gender">Gender:</label><br>
+  <select id="gender" name="gender">
+    <option value="male">Male</option>
+    <option value="female">Female</option>
+  </select><br><br>
+  <input type="submit" value="Submit">
+</form>
